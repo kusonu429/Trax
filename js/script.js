ab63142d66dfd534a665856bf28db625b8ca20d5
Sonu <98530388+kusonu429@users.noreply.github.com>
2022-11-29 18:48:15 +0530

    desktop done
    desktop done

---
+++ js/script.js
@@ -0,0 +1,29 @@
+
+
+
+//...................Sticky Navbar starts ...............//
+$(function(){
+
+    $(window).on('scroll', function(){
+  
+      if ($(window).scrollTop ()){
+  
+          $('nav').addClass('nav-white'); 
+      }
+  
+  
+      else{
+          $('nav').removeClass('nav-white');
+      }
+  
+  }) 
+  
+  
+   
+  }); 
+  
+  
+  
+  
+  //...................Sticky Navbar Ends ...............//
+  
