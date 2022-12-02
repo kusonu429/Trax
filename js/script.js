f15b2386fc121652f772aa43515188507842e959
Sonu <98530388+kusonu429@users.noreply.github.com>
2022-12-02 20:21:38 +0530

    update
    done

--- js/script.js
+++ js/script.js
@@ -8,22 +8,50 @@ $(function(){
   
       if ($(window).scrollTop ()){
   
-          $('nav').addClass('nav-white'); 
+          $('.navbar').addClass('nav-white'); 
       }
   
   
       else{
-          $('nav').removeClass('nav-white');
+          $('.navbar').removeClass('nav-white');
       }
   
   }) 
   
   
    
-  }); 
+  });  
   
   
   
   
   //...................Sticky Navbar Ends ...............//
+
+
+//   logo add remove  starts here 
+ 
+$(function(){ 
+
+    $(window).on('scroll', function(){
+  
+      if ($(window).scrollTop ()){
+  
+          $('.logo').addClass('d-none');   
+          
+      } 
+  
+  
+      else{ 
+          $('.logos').removeClass('d-none'); 
+        
+      }
+  
+  }) 
+  
+  
+   
+  });  
+  
+
+
   
